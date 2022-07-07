6b5b70b3ffc2137a445240138a0c65c2080c9734
amir <galimzyanoffam@yandex.ru>
2022-07-08 00:19:23 +0300

    am-api

--- database/migrations/2022_07_07_210629_create_tasks_table.php
+++ database/migrations/2022_07_07_210629_create_tasks_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('dasks', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('card_id')->constrained();
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('dasks');
+        Schema::dropIfExists('tasks');
     }
 };
