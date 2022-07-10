6806d3ac326834fb63ad7a0b97bdf18692f40425
amir <galimzyanoffam@yandex.ru>
2022-07-10 21:58:13 +0300

    откат сборщика фронта и минимальная настройка vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+window.Vue = require('vue').default;
+
+import Vue from 'vue'
+
+Vue.component('app', require('./components/App').default)
+
+const app = new Vue({
+    el: '#app',
+})
---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+                content="width-device-width"
+    >
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+   <title>Trello analog</title>
+    <body>
+        <div id="app">
+            <app></app>
+        </div>
+    <script src="{{mix('js/app.js')}}"></script>
+    </body>
+</html>
